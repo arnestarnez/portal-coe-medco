0b904418b60239b0c034a71f072d0f29d87a81f5
DESKTOP-KGE8ME5\T460 <deni.sumarno@ogya.co.id>
2022-08-21 22:38:43 +0700

    index.js dan Routes.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+export { default as Dashboard } from './Dashboard';
+export { default as Home } from './Home';
+export { default as MiaCluster } from './MiaCluster';
+export { default as MiaSubmittion } from './MiaSubmittion';
+export { default as MiaReviewing } from './MiaReviewing';
+export { default as MiaJudging } from './MiaJudging';
+export { default as MiaAward } from './MiaAward';
+export { default as MiaDashboard } from './MiaDashboard';
 
-import * as serviceWorker from './serviceWorker';
-import App from './App';
-import {createStore} from 'redux'
-import { Provider } from 'react-redux';
+export { default as NotFound } from './NotFound';
+export { default as SignIn } from './SignIn';
+export { default as Signout } from './Signout';
+export { default as SignUp } from './SignUp';
+export {default as Profile } from './Profile';
 
-const globalState={
-    orgData:[],
-}
-const rootReducer=(state=globalState,action)=>{
-    //return state;
-  
-    switch(action.type)
-    {
-            case 'UBAH_DATA' :
-                alert(action.type)
-                return{
-                    ... state,                   //
-                     orgData:action.newValue
-                     //salary:action.newValue
- 
-                    
-                }
-                default :
-                return state;
-        
-    } 
-}
-//    const storeRedux=createStore(rootReducer)
-    const storeRedux=createStore(rootReducer)
+export {default as VisitorUser} from './Dashboard/components/VisitorUser/';
+export {default as KelurahanList} from './KelurahanList'
+export {default as KecamatanList} from './KecamatanList'
+export {default as KabupatenList} from './KabupatenList'
+export {default as VuserList} from './VuserList'
+export {default as SettingList} from './SettingList'
+export {default as KelompokDataList} from './KelompokDataList'
+export {default as LaporanSensusIDList} from './LaporanSensusID'
+export {default as TargetKkList} from './TargetKkList'
+export {default as LaporanSensusPerKab} from './LaporanSensusPerKab'
+export {default as LaporanSensusPerProv} from './LaporanSensusPerProv'
+export {default as UserAccessSurveyList} from './UserAccessSurveyList'
+export {default as LaporanTargetRealisasiID} from './LaporanTargetRealisasiID'
+export {default as LaporanTargetRealisasiPerprov} from './LaporanTargetRealisasiPerprov'
+export {default as LaporanTargetRealisasiPerkab} from './LaporanTargetRealisasiPerkab'
+export {default as LaporanKbId} from './LaporanKbId'
+export {default as LaporanKbPerProv} from './LaporanKbPerProv'
+export {default as LaporanKbPerkab} from './LaporanKbPerkab'
+// export {default as RtList} from './RtList'
+// export {default as RwList} from './RwList'
 
-ReactDOM.render(<Provider store={storeRedux}><App /></Provider>, document.getElementById('root'));
 
-serviceWorker.unregister();
+
+
